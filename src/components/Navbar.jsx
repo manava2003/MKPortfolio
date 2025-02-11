32c193e6e3e2591feab639bf8564dcefa3878ae1
Lalit Singh <lalit284546@gmail.com>
2025-02-11 13:26:55 +0530

    navbar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import React from "react";
 export const Navbar = () => {
   return (
     <div className=" bg-[#2424248C] gap-[40px] rounded-[10px] font-[Poppins] p-5 backdrop-blur-md fixed  w-[477px] h-[40px] z-30 flex text-white justify-between items-center">
-      {/* item1 */}
       <button className="flex items-center justify-center ">
         <span>Menu</span>
       </button>
